8447a6cd40499e8b58e08263de4c5a15a22d55dd
Richard Dallaway <richard@dallaway.com>
2010-05-02 18:07:23 +0100

    Copyright update

--- src/main/scala/com/dallaway/tidetimes/source/TideSource.scala
+++ src/main/scala/com/dallaway/tidetimes/source/TideSource.scala
@@ -1,7 +1,7 @@
 package com.dallaway.tidetimes.source
 
 /*
-  Copyright 2009 Richard Dallaway
+  Copyright 2009-2010 Richard Dallaway
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
